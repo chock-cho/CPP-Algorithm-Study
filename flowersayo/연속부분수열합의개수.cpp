863d02ab927b0993a4782e080e9b849b3f2d656b
flowersayo <flowersayo0811@ewhain.net>
2023-07-28 14:42:11 +0900

    김서연 / 20230728 / [프로그래머스] 연속부분수열합의개수 refactor

--- flowersayo/연속부분수열합의개수.cpp
+++ flowersayo/연속부분수열합의개수.cpp
@@ -12,20 +12,16 @@ int solution(vector<int> elements) {
     
     set<int> s;
     
-    int size= elements.size();
+    int size = elements.size();
     
-    for(int i=1; i<=size;i++){ // i : 선택하는 원소의 크기
-        
-        for(int j=0;j<size;j++){ // j : 덧셈의 시작점
-            
-            int sum =0;
-            for(int k=j;k<j+i;k++){ // j 부터 i 개 만큼 더함 
-                sum+=elements[k % size];              
-            }
-            s.insert(sum);
-         
-        }
-        
-    }
+   for(int i=0;i<size;i++){ // i : 덧셈 시작점
+       
+       int sum =0;
+       for(int j=0;j<size;j++){ // i 부터 i+j까지 더해가는 과정에서 나온 중간 합들을 모두 insert 
+           sum+=elements[(i+j)%size]; 
+           s.insert(sum);
+       }
+       
+   }
     return s.size();
 }
