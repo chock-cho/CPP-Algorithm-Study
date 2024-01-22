5a897a0fd31b78023fb32462891829ddf4b497d2
베뉴 <113707388+chock-cho@users.noreply.github.com>
2024-01-22 20:51:46 +0900

    신수정 / 20240107 / [LeetCode] 3. K closet Points to Origin

---
+++ Sujung/3_KClosetPointstoOrigin.cpp
@@ -0,0 +1,25 @@
+/* K closet points to origin - 정렬, 우선순위 큐 커스텀하여 푸는 문제였습니다. */
+class distanceCmp {
+    // 커스텀 정렬 클래스 생성
+public:
+    bool operator() (vector<int>& a, vector<int>& b){
+        return (a[0] * a[0] + a[1] * a[1]) > (b[0] * b[0] + b[1] * b[1]);
+    }
+};
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, distanceCmp> pq; // 커스텀 정렬 기준 최소 히프 생성
+        for(auto p: points) {
+            pq.push(p);
+        }
+
+        vector<vector<int>> ret;
+        while(!pq.empty() && k != 0){
+            ret.push_back(pq.top());
+            pq.pop();
+            k--;
+        }
+        return ret;
+    }
+};
